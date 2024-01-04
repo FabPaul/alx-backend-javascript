c10db969b584332a71047536afd47c242db0cf70
FabPaul <paulfopa0@gmail.com>
2024-01-04 18:14:44 +0100

    primitive

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Convert object to primitive value
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this.location;
+  }
+}

06abc9a1ebdff0b723f5d3b426d5beedeac3d616
FabPaul <paulfopa0@gmail.com>
2024-01-04 18:35:35 +0100

    Better sysntax for symbol

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,25 +4,8 @@ export default class Airport {
     this._code = code;
   }
 
-  // Getter and setter
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
   // Symbol
-  toString() {
-    return `[object ${this.code}]`;
+  get [Symbol.toStringTag]() {
+    return `[object ${this._code}]`;
   }
 }

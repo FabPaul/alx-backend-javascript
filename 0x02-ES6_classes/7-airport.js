0156a3267531e287073dcb2dc1daf55ec5491454
FabPaul <paulfopa0@gmail.com>
2024-01-04 17:57:36 +0100

    getter and setter for symbol

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,6 +4,23 @@ export default class Airport {
     this._code = code;
   }
 
+  // Getter and setter
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
   // Symbol
   toString() {
     return `[object ${this.code}]`;

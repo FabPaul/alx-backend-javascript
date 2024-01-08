e17b012f720ae41648e52aa502705067d16ac6d5
FabPaul <paulfopa0@gmail.com>
2024-01-08 17:30:11 +0100

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,15 @@
 export default function updateStudentGradeByCity(listOfObjects, city, newGrades) {
   const cityOfStudent = listOfObjects.filter((student) => student.location === city);
 
-  const studentUpdate = cityOfStudent.map((student) => {
-    const grades = newGrades.find((grade) => grade.idOfStudent === student.id);
+  const updateStudents = cityOfStudent.map((student) => {
+    const grades = newGrades.find((grade) => grade.studentId === student.id);
 
-    const updateGrade = grades ? grades.grade : 'N/A';
+    const finalGrades = grades ? grades.grade : 'N/A';
 
     return {
       ...student,
-      grade: updateGrade,
+      grade: finalGrades,
     };
   });
-  return studentUpdate;
+  return updateStudents;
 }

fefe4a4ef4ea48ddaad51969980dd3648d823a6e
FabPaul <paulfopa0@gmail.com>
2024-02-27 13:26:23 +0100

    corrected task 2, confused with task 3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -22,14 +22,12 @@ function countStudents(path) {
       }
     });
 
-    console.log('After!');
     console.log(`Number of students: ${validLines.length}`);
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
     }
-    console.log('Done!');
   } catch (error) {
     throw new Error('Cannot load the database');
   }

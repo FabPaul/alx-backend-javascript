29fd2ebaeddc825e7b5962afc0f5fc775e8d757b
FabPaul <paulfopa0@gmail.com>
2024-02-29 15:45:32 +0100

    reverting

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,21 +2,20 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', function() {
-    it('Correct status code', function testcode() {
-        res = request.get('http://localhost:7865/', (err, res, data) => {
-          expect(res.statusCode).to.be.equal(200);
+    it('Correct status code', function() {
+        res = request.get('http://localhost:7865', (err, res, data) => {
+            expect(res.statusCode).to.be.equal(200);
         });
-      });
+    });
+    it('Correct result', function() {
+        res = request.get('http://localhost:7865', (err, res, data) => {
+            expect(data).to.be.equal('Welcome to the payment system');
+        });
+    });
     
-      it('Correct data', function testcode() {
-        res = request.get('http://localhost:7865/', (err, res, data) => {
-          expect(data).to.be.equal('Welcome to the payment system');
+     it('Other', function() {
+        res = request.get('http://localhost:7865', (err, res, data) => {
+            expect(data.length).to.be.equal(29)
         });
-      });
-    
-      it('Other', function testcode() {
-        res = request.get('http://localhost:7865/', (err, res, data) => {
-          expect(data.length).to.be.equal(29);
-        });
-      });
-    })
+    });
+});

10c86c162c53fc61efa14180b75a3908f7bd70f3
FabPaul <paulfopa0@gmail.com>
2024-02-29 12:51:57 +0100

    stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,17 +6,16 @@ const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./4-payment.js');
 const stub = sinon.stub(Utils, 'calculateNumber');
 
-describe('sendPaymentRequestToApi', function() {
-    it('Makes sure stubs is being called and used', function() {
-        stub.withArgs('SUM', 100, 20).return(10)
-        sendPaymentRequestToApi(100, 20);
-        expect(Utils.calculateNumber('SUM', 100, 20)).to.be.equal(10);
-    });
+describe('sendPaymentRequestToApi', function () {
+    it('Makes sure stubs is being called and used', function () {
+    stub.withArgs('SUM', 100, 20).returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber('SUM', 100, 20)).to.be.equal(10);
+    })
 
-    it('Verify that console.log is logging the correct message', function() {
-        const spy = sinon.spy(console, 'log');
-        sendPaymentRequestToApi(100, 20);
-        expect(console.log.calledWith('The total is: 10')).to.be.true;
-        console.log.restore();
+    it('Verify that console.log is logging the correct message', function () {
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnceWithExactly(spy, 'The total is: 10');
     });
 });

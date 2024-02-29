ac8397c72d36ce12347e596d985ba67992f925b8
FabPaul <paulfopa0@gmail.com>
2024-02-29 16:57:01 +0100

    work now?

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,21 +1,26 @@
-const request = require('request');
 const { expect } = require('chai');
+const request = require('request');
+const sinon = require('sinon');
 
-describe('Index page', function() {
-    it('Correct status code', function() {
-        res = request.get('http://localhost:7865', (err, res, data) => {
-            expect(res.statusCode).to.be.equal(200);
-        });
+describe('Index page', function testApi() {
+  beforeEach(() => {
+    sinon.restore();
+  });
+  it('Test Correct status code', function testcode() {
+    res = request.get('http://localhost:7865/', (err, res, data) => {
+      expect(res.statusCode).to.be.equal(200);
     });
-    it('Correct result', function() {
-        res = request.get('http://localhost:7865', (err, res, data) => {
-            expect(data).to.be.equal('Welcome to the payment system');
-        });
-    });
+  });
 
-     it('Other', function() {
-        res = request.get('http://localhost:7865', (err, res, data) => {
-            expect(data.length).to.be.equal(29)
-        });
+  it('Correct result', function testcode() {
+    res = request.get('http://localhost:7865/', (err, res, data) => {
+      expect(data).to.be.equal('Welcome to the payment system');
     });
-});
+  });
+
+  it('Test others', function testcode() {
+    res = request.get('http://localhost:7865/', (err, res, data) => {
+      expect(data.length).to.be.equal(29);
+    });
+  });
+})

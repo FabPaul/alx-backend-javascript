819e828c74fe1e5ad1698cfe079621224efd4a74
FabPaul <paulfopa0@gmail.com>
2024-02-29 15:30:28 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,23 +1,20 @@
 const request = require('request');
 const { expect } = require('chai');
 
-const hostname = '127.0.0.1';
-const port = 7865;
-
 describe('Index page', function() {
     it('Correct status code', function() {
-        res = request.get(`http://${hostname}:${port}`, (err, res, data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(res.statusCode).to.be.equal(200);
         });
     });
     it('Correct result', function() {
-        res = request.get(`http://${hostname}:${port}`, (err, res, data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(data).to.be.equal('Welcome to the payment system');
         });
     });
     
      it('Other', function() {
-        res = request.get(`http://${hostname}:${port}`, (err, res, data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(data.length).to.be.equal(29)
         });
     });

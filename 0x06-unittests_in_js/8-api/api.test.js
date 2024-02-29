86d7db43417529ed46091e581af5f70482fc8d84
FabPaul <paulfopa0@gmail.com>
2024-02-29 15:33:16 +0100

    added restore

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,12 +1,20 @@
 const request = require('request');
 const { expect } = require('chai');
+const sinon = require('sinon');
+const { beforeEach } = require('mocha');
+
 
 describe('Index page', function() {
+    beforeEach(() => {
+        sinon.restore();
+    });
+
     it('Correct status code', function() {
         res = request.get('http://localhost:7865', (err, res, data) => {
             expect(res.statusCode).to.be.equal(200);
         });
     });
+    
     it('Correct result', function() {
         res = request.get('http://localhost:7865', (err, res, data) => {
             expect(data).to.be.equal('Welcome to the payment system');

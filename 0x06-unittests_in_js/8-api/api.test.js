b4484e66a23fd7f934c867a1490d3c847d2df165
FabPaul <paulfopa0@gmail.com>
2024-02-29 15:49:04 +0100

    reverting

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,18 +3,18 @@ const { expect } = require('chai');
 
 describe('Index page', function() {
     it('Correct status code', function() {
-        res = request.get('http://localhost:7865', (_err, res, _data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(res.statusCode).to.be.equal(200);
         });
     });
     it('Correct result', function() {
-        res = request.get('http://localhost:7865', (_err, _res, data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(data).to.be.equal('Welcome to the payment system');
         });
     });
     
      it('Other', function() {
-        res = request.get('http://localhost:7865', (_err, _res, data) => {
+        res = request.get('http://localhost:7865', (err, res, data) => {
             expect(data.length).to.be.equal(29)
         });
     });
